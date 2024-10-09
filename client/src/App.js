676e33c806a6b59b75b583022c17aa6b95960047
stalin670 <amitalvin01@gmail.com>
2024-10-09 18:52:23 +0530

    adding code editor, input and output functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,118 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Editor from "@monaco-editor/react";
+import Axios from "axios";
+import spinner from "./spinner.svg";
 
 function App() {
+  const [userCode, setUserCode] = useState(``);
+
+  const [userLang, setUserLang] = useState("python");
+
+  const [userTheme, setUserTheme] = useState("vs-dark");
+
+  const [fontSize, setFontSize] = useState(20);
+
+  const [userInput, setUserInput] = useState("");
+
+  const [userOutput, setUserOutput] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const options = {
+    fontSize: fontSize,
+  };
+
+  function compile() {
+    setLoading(true);
+    if (userCode === ``) {
+      return;
+    }
+
+    // Post Compile API request
+    Axios.post(`http://localhost:8000/compile`, {
+      code: userCode,
+      language: userLang,
+      input: userInput,
+    })
+      .then((res) => {
+        setUserOutput(res.data.stdout || res.data.stderr);
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setUserOutput(
+          "Error: " + (err.response ? err.response.data.error : err.message)
+        );
+        setLoading(false);
+      });
+  }
+
+  function clearOutput() {
+    setUserOutput("");
+  }
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar
+        userLang={userLang}
+        setUserLang={setUserLang}
+        userTheme={userTheme}
+        setUserTheme={setUserTheme}
+        fontSize={fontSize}
+        setFontSize={setFontSize}
+      />
+
+      <div className="main">
+        <div className="left-container">
+          <Editor
+            options={options}
+            height="calc(100vh - 50px)"
+            width="100%"
+            theme={userTheme}
+            language={userLang}
+            defaultLanguage="python"
+            defaultValue="# Enter your code here"
+            onChange={(value) => {
+              setUserCode(value);
+            }}
+          />
+          <button className="run-btn" onClick={() => compile()}>
+            Run
+          </button>
+        </div>
+
+        <div className="right-container">
+          <h4>Input:</h4>
+          <div className="input-box">
+            <textarea
+              id="code-inp"
+              onChange={(e) => setUserInput(e.target.value)}
+            ></textarea>
+          </div>
+          <h4>Output:</h4>
+          {loading ? (
+            <div className="spinner-box">
+              <img src={spinner} alt="Loading..." />
+            </div>
+          ) : (
+            <div className="output-box">
+              <pre>{userOutput}</pre>
+              <button
+                onClick={() => {
+                  clearOutput();
+                }}
+                className="clear-btn"
+              >
+                Clear
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
